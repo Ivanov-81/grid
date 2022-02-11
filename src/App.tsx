d05c419e43f817fa8ba7b310bc9b729fe7bf83c2
Oleg Ivanov <gmiolegv@gmail.com>
2022-02-11 11:58:34 +0300

    Clear libs & App

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import React, {useEffect, useRef} from 'react';
 import clsx from 'clsx';
-import {makeStyles, Theme} from "@material-ui/core";
 
 import './App.css';
-
-const useStyles = makeStyles((theme: Theme) => ({
-
-}));
-
 
 function App() {
 
     const resInput = useRef(null)
-    const classes = useStyles();
 
     const [value, setValue] = React.useState(0);
 
