2b4a279e64d589c91038ba6d8a87dd1c82fe0fe8
Oleg Ivanov <gmiolegv@gmail.com>
2022-02-11 14:06:25 +0300

    grid

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,22 @@ function App() {
     }, [])
 
     return (
-        <div className="App"/>
+        <>
+            <div className="wrapper">
+                <div className="one">One</div>
+                <div className="two">Two</div>
+                <div className="three">Three</div>
+                <div className="four">Four</div>
+                <div className="five">Five</div>
+                <div className="six">Six</div>
+            </div>
+            <div className="wrapper">
+                <div className="box1">One</div>
+                <div className="box2">Two</div>
+                <div className="box3">Three</div>
+                <div className="box4">Four</div>
+            </div>
+        </>
     );
 }
 
